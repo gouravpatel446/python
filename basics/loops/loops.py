b88098caf88a0770e04c521e911ab5bbfd7d7774
Gourav Patel <gouravpatel446@gmail.com>
2026-04-18 06:47:50 +0530

    functional loops in python

---
+++ basics/loops/loops.py
@@ -0,0 +1,35 @@
+#normal if else
+age = 18
+
+if age >= 18: #colon lagana hai 
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+for i in range(1,6):
+    print(i)
+
+
+#iterating over a string
+str = "Hello, World!"
+for i in str:
+    print(i)
+
+#iterating over a list
+list_1 = [1,2,3,4,5]
+for i in list_1:
+    print(i)
+
+list_2 = ["apple", "banana","kela"]
+for i in list_2:
+    print(i)
+
+#iterating over a tuple using loopp
+tuple_1 = (1, 2, 3, 4, 5)
+for i in tuple_1:
+    print(i)
+
+#iterating over a dictionary using loop
+dict_1 = {"name": "Alice", "age": 30, "city": "New York"}
+print(i , ":",dict_1[i])
